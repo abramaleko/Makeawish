55d07cad14b494b2642acf5f58f21d054efc04d2
abramaleko <abrahammaleko@gmail.com>
2021-04-25 17:20:54 +0300

    show only approved requests

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -30,7 +30,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $wishes=Wishes::where('status','Pending')->orderBy('id','desc')->paginate(3);
+        $wishes=Wishes::where('status','Pending wisha')->orderBy('id','desc')->paginate(3);
         return view('home',['wishes'=> $wishes]);
     }
 
