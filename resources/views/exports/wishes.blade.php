d8dbb73af920a32ce61788bcc8febfc32d8dccac
Abraham Maleko <abrahammaleko@gmail.com>
2022-03-02 15:21:51 +0300

    added grantee details in excel

--- resources/views/exports/wishes.blade.php
+++ resources/views/exports/wishes.blade.php
@@ -6,20 +6,32 @@
         </tr>
         <br><br>
       <tr>
-        <th scope="col">Name</th>
-        <th scope="col">Email</th>
+        <th scope="col">S/N</th>
+        <th scope="col">Requestee Name</th>
+        <th scope="col">Requestee Email</th>
+        <th scope="col">Requestee Wish</th>
         <th scope="col">Amount</th>
         <th scope="col">Status</th>
-        <th scope="col">Date</th>
+        <th scope="col">Granted By</th>
+        <th scope="col">Grantee Email</th>
+        <th scope="col">Grantee Phone #</th>
+
+
+        <th scope="col">Requested Date</th>
       </tr>
     </thead>
     <tbody>
         @foreach ($wishes as $wish)
         <tr>
+            <td>{{$loop->iteration}}</td>
             <td>{{$wish->name}}</td>
             <td>{{$wish->email}}</td>
+            <td>{{$wish->description}}</td>
             <td>{{$wish->amount}}</td>
             <td>{{$wish->status}}</td>
+            <td>{{$wish->grant_name}}</td>
+            <td>{{$wish->grant_email}}</td>
+            <td>{{$wish->grant_phone_number}}</td>
             <td>{{$wish->created_at->format('d M Y')}}</td>
         </tr>
         @endforeach
